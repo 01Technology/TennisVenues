922241f6ff50d0dc0fa755089ca6c3e11b615ca1
Roshan Bhandari <bhandariroshan60@gmail.com>
2020-12-18 18:35:07 +0545

    Promotion Tab added

--- src/navigations/Tabs/Promotions.js
+++ src/navigations/Tabs/Promotions.js
@@ -1,160 +1,125 @@
 import React, { Component } from 'react';
-import { View, StyleSheet,ScrollView, TextInput, Image, Dimensions, TouchableOpacity, FlatList} from 'react-native';
+import { View, StyleSheet, ScrollView, TextInput, Image, Dimensions, TouchableOpacity,ImageBackground} from 'react-native';
 import { Avatar, Card, Button,  } from 'react-native-elements';
 
 import colors from '../../Config/colors';
 import Text from '../../Config/CustomFont';
+import urls from '../../utils/urls';
 
 import { Ionicons } from '@expo/vector-icons'; 
 
 export default class Plus extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dataSource: [],
-      images:[],
-    };
-  }
-
-  //fetching data from server
-  componentDidMount(){
-
-    const response = fetch('https://jsonplaceholder.typicode.com/photos')
-    .then((response) => response.json())
-    .then((responseJson) => {
-      this.setState({
-        dataSource: responseJson,
-      })
-    })
-  }
-
-  statePressed = () => {
-    console.log("STATE PRESSED");
-  }
-
-  subrubPressed = () => {
-    console.log("subrub PRESSED");
-  }
-
-  typeofcourtPressed = () => {
-    console.log("type of court PRESSED");
-  }
-
-  booktheCourt = ({item}) => {
-    //console.log(item.name);
-    this.props.navigation.navigate("BookTheCourt",{name: item.title, email:item.title});
-  }
-
-  //render flatlist item
-  _renderItem = ({item, index}) => {
-    return(
-        <View style = {{flexDirection:'row', marginHorizontal:20, marginVertical:30}}>
-            <View style = {{flexDirection:'row'}}>
-                <Image source = {{uri:item.url}} style = {{width:100, height:70, alignSelf:'baseline'}} /> 
-                <View>
-                  <Ionicons name="location" size={24} color={colors.secondary} />
-                </View>   
-            </View>
-            <View style = {{width:Dimensions.get('window').height/7, height:20}}>
-                <View>
-                  <Text type = "bold" style = {{fontSize:12,}}>{item.title}</Text>
-                </View>
-                <View>
-                  <Text style = {{fontSize:8, width:'100%'}}>{item.title}</Text>
-                </View>
-                <View style = {{flexDirection:'row',}}>
-                    <Image source = {require('../../assets/Icons/BALL-2-391.png')} style ={{width:20, height:20}} />
-                    <Image source = {require('../../assets/Icons/BALL-2-391.png')} style ={{width:20, height:20}} />
-                    <Image source = {require('../../assets/Icons/BALL-2-391.png')} style ={{width:20, height:20}} />
-                    <Image source = {require('../../assets/Icons/BALL-2-391.png')} style ={{width:20, height:20}} />
-                    <Image source = {require('../../assets/Icons/BALL-531.png')} style ={{width:20, height:20}} />
-                </View>
-            </View>
-            <View style = {{justifyContent:'center'}}>
-                <Button 
-                  containerStyle={{ paddingLeft:10, paddingRight:10, paddingTop:5, paddingBottom:5, }}
-                  title="BOOK THIS COURT" 
-                  buttonStyle={{ width:100, height:25, backgroundColor: colors.primary, borderRadius:50, borderColor:colors.primary, borderWidth:2 }}  
-                  onPress={()=> this.booktheCourt({item})}
-                  titleStyle = {{fontSize:8, color:colors.white}}
-                />
-              </View>
-        </View>
-    )
-  }
 
   render() {
-    const count = Object.keys(this.state.dataSource).length
     return (
-      <View style={{backgroundColor:colors.white, }}>
+    <ScrollView>
+      <View style = {styles.container}>
         <View style = {styles.top}>
-            <View>
-                <TextInput 
-                    placeholder = "SEARCH FOR VENUES" 
-                    onChangeText = {(val) => {this.setState({email: val})}} 
-                    style = { styles.input } 
-                />
-            </View>
-            <View>
-                <Ionicons name="search" size={30} color={colors.primary} />
-            </View>   
-            <View>
-                <Ionicons name="notifications" size={30} color={colors.primary}/>
-            </View>           
-        </View>
-        <View style = {{flexDirection:'row',marginHorizontal:20, alignSelf:'flex-start'}}>
-          <Text type = "bold" style = {{ color:colors.primary, }}>FILTER</Text>
-          <Ionicons style = {{marginLeft:10}} name="filter" size={18} color={colors.primary}/>
-        </View>
-        <View style = {{ flexDirection:'row', justifyContent:'space-between', alignSelf:'stretch'}}>
           <View>
-            <Button 
-                containerStyle={{ paddingLeft:10, paddingRight:10, paddingTop:5, paddingBottom:5, }}
-                title="STATE" 
-                buttonStyle={{ width:100, height:35, backgroundColor: colors.smokewhite, borderRadius:50, borderColor:colors.primary, borderWidth:2 }}  
-                onPress={() => { this.statePressed(); }}
-                titleStyle = {{fontSize:8, color:colors.primary}}
-              />
+            <TextInput 
+                placeholder = "SEARCH FOR VENUES" 
+                onChangeText = {(val) => {this.setState({email: val})}} 
+                style = { styles.input } 
+            />
+          </View> 
+          <View>
+              <Ionicons name="search" size={30} color={colors.white} />
           </View>
           <View>
-            <Button 
-                containerStyle={{ paddingLeft:10, paddingRight:10, paddingTop:5, paddingBottom:5, }}
-                title="SUBURB" 
-                buttonStyle={{ width:100, height:35, backgroundColor: colors.white, borderRadius:50, borderColor:colors.primary, borderWidth:2 }}  
-                onPress={() => { this.subrubPressed(); }}
-                titleStyle = {{fontSize:8, color:colors.primary}}
-              />
+              <Ionicons name="notifications" size={30} color={colors.white}/>
+          </View>   
+        </View>
+        <View style = {styles.promotionsforyouSection}>
+          <View>
+            <Text type = "bold" style = {styles.promotions}>PROMOTIONS</Text>
           </View>
           <View>
-            <Button 
-                containerStyle={{ paddingLeft:10, paddingRight:10, paddingTop:5, paddingBottom:5, }}
-                title="TYPE OF COURT" 
-                buttonStyle={{ width:100, height:35, backgroundColor: colors.white, borderRadius:50, borderColor:colors.primary, borderWidth:2 }}  
-                onPress={() => { this.typeofcourtPressed(); }}
-                titleStyle = {{fontSize:8, color:colors.primary}}
-              />
+              <Text type = "bold" style = {styles.foryou}>FOR YOU!</Text>
           </View>
         </View>
 
-        <View>
-          <Text style = {{marginHorizontal:20}}>{count} VENUES NEAR SUBRUB</Text>
-        </View>
-        <View style = {{ justifyContent:'space-between'}}>
-              <FlatList
-                  data = {this.state.dataSource}
-                  renderItem = {this._renderItem}
-                  keyExtractor = {(item, index) => index.toString()}
-                  ItemSeparatorComponent = {(props) => 
-                      <View style = {{width:"90%", height:1, backgroundColor:colors.primary, marginHorizontal:20}} />
-                    }
-              />
+        <View style = {styles.sectionContainer}>
+          <View style = {styles.boxRowContainer}>
+            <ImageBackground source={require('../../assets/Images/Rectangle75.png')} style = {styles.box}  imageStyle={styles.boxRadius}>
+              <View style = {styles.boxTextContainer}>
+                <Text type = "bold" style = {styles.textOffer}>Hey There </Text>
+                <Text type = "thin" style = {styles.textOfferContent}>Hey there this iss lorem ipsums </Text>
+              </View>
+            </ImageBackground>
+            <ImageBackground source={require('../../assets/Images/Rectangle76.png')} style = {styles.box}  imageStyle={styles.boxRadius}>
+              <View style = {styles.boxTextContainer}>
+                <Text type = "bold" style = {styles.textOffer}>Hey there </Text>
+                <Text type = "thin" style = {styles.textOfferContent}>Hey there this iss lorem ipsums </Text>
+              </View>
+            </ImageBackground>
+          </View>
+          <View style = {styles.boxRowContainer}>
+            <ImageBackground source={require('../../assets/Images/Rectangle77.png')} style = {styles.box}  imageStyle={styles.boxRadius}>
+              <View style = {styles.boxTextContainer}>
+                <Text type = "bold" style = {styles.textOffer}>Hey there </Text>
+                <Text type = "thin" style = {styles.textOfferContent}>Hey there this iss lorem ipsums </Text>
+              </View>
+            </ImageBackground>
+            <ImageBackground source={require('../../assets/Images/Rectangle78.png')} style = {styles.box}  imageStyle={styles.boxRadius}>
+              <View style = {styles.boxTextContainer}>
+                <Text type = "bold" style = {styles.textOffer}>Hey there </Text>
+                <Text type = "thin" style = {styles.textOfferContent}>Hey there this iss lorem ipsums </Text>
+              </View>
+            </ImageBackground>
+          </View>
+          <View style = {styles.boxRowContainer}>
+            <ImageBackground source={require('../../assets/Images/Rectangle79.png')} style = {styles.box}  imageStyle={styles.boxRadius}>
+              <View style = {styles.boxTextContainer}>
+                <Text type = "bold" style = {styles.textOffer}>Hey there </Text>
+                <Text type = "thin" style = {styles.textOfferContent}>Hey there this iss lorem ipsums </Text>
+              </View>
+            </ImageBackground>
+            <ImageBackground source={require('../../assets/Images/Rectangle80.png')} style = {styles.box}  imageStyle={styles.boxRadius}>
+              <View style = {styles.boxTextContainer}>
+                <Text type = "bold" style = {styles.textOffer}>Hey there </Text>
+                <Text type = "thin" style = {styles.textOfferContent}>Hey there this iss lorem ipsums </Text>
+              </View>
+            </ImageBackground>
+          </View>
         </View>
       </View>
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  boxRowContainer:{
+    flexDirection:'row',
+    justifyContent:'space-between',
+    marginBottom:20
+  },
+  box:{
+    width: Dimensions.get('window').width/2.5,
+    height: Dimensions.get('window').height/4.5,
+    backgroundColor:'red',
+    justifyContent:'flex-end',
+    alignItems:'center',
+    borderRadius: 6, 
+    overflow: 'hidden'
+  },
+  boxRadius:{
+    borderRadius:12
+  },
+  boxTextContainer:{
+    backgroundColor:colors.secondary,
+    opacity:0.7,
+    paddingHorizontal:10,
+    paddingVertical:5,
+    justifyContent:'space-between',
+    maxHeight:50,
+  },
+  container:{
+    backgroundColor:colors.primary
+  },
+  foryou:{
+    fontSize:28, color:colors.white, marginLeft:20,
+  },
   top:{
     marginTop:25,
     padding:10,
@@ -170,15 +135,71 @@ const styles = StyleSheet.create({
     borderWidth: 0.5,
     height:35, 
     paddingHorizontal:30, 
-    backgroundColor: colors.white,
     fontSize: 18,
-    marginLeft:0
+    marginLeft:0,
+    backgroundColor:colors.white,
   },
-  flatLIst:{ 
-    flexDirection:'row', 
-    justifyContent:'space-between',
-    backgroundColor:'#fff',
-    elevation: 2, 
-    marginHorizontal:20,
+  image:{
+    resizeMode:"cover", width:169, height:101, borderRadius: 12,shadowColor: "#000", 
   },
+  topCurve:{
+    backgroundColor:colors.primary, 
+    width:'100%', 
+    height:'40%',
+  },  
+  textOffer:{
+    color:colors.white
+  },
+  textOfferContent:{
+    fontSize:10
+  },
+  bottomCurve:{
+
+  },
+  promotionsforyouSection:{
+    marginRight:20,
+    alignItems:'flex-end',
+    marginVertical:20,
+  },
+  promotions:{
+    fontSize:28, 
+    color:colors.white, 
+    marginLeft:20, 
+    marginBottom:10,
+  },
+  sectionContainer:{
+    backgroundColor:colors.white,
+    paddingHorizontal:20,
+    paddingVertical:20,
+    justifyContent:'space-between'
+  }
+  /*
+  triangle: {
+    position:'absolute',
+    width: 0,
+    height: 0,
+    backgroundColor: "transparent",
+    borderStyle: "solid",
+    borderLeftWidth: Dimensions.get('window').height/2,
+    borderBottomWidth:Dimensions.get('window').width,
+    borderLeftColor: "transparent",
+    borderRightColor: "transparent",
+    borderBottomColor: colors.secondary,
+    transform: [{ rotate: "90deg" }],
+  },
+  
+  triangle2: {
+    position:'absolute',
+    width: 0,
+    height: 0,
+    backgroundColor: "transparent",
+    borderStyle: "solid",
+    borderLeftWidth: Dimensions.get('window').height/2,
+    borderBottomWidth:Dimensions.get('window').width,
+    borderLeftColor: "transparent",
+    borderRightColor: "transparent",
+    borderBottomColor: colors.secondary,
+    transform: [{ rotate: "90deg" }],
+  },
+  */
 })
